305e48d37290713dedaa50d9ec64d44d53941267
Heiher <r@hev.cc>
2016-10-09 23:27:58 +0800

    Android: Update JNI method defination and class name.

--- src/hev-jni.c
+++ src/hev-jni.c
@@ -30,8 +30,8 @@ static void native_stop_service (JNIEnv *env, jobject thiz);
 
 static JNINativeMethod native_methods[] =
 {
-	{ "NativeStartService", "(Ljava/lang/String;ILjava/lang/String;)V", (void *) native_start_service },
-	{ "NativeStopService", "()V", (void *) native_stop_service },
+	{ "DNSFwdStartService", "(Ljava/lang/String;ILjava/lang/String;)V", (void *) native_start_service },
+	{ "DNSFwdStopService", "()V", (void *) native_stop_service },
 };
 
 static void
@@ -51,7 +51,7 @@ JNI_OnLoad (JavaVM *vm, void *reserved)
 		return 0;
 	}
 
-	klass = (*env)->FindClass (env, "hev/htproxy/DNSFwdService");
+	klass = (*env)->FindClass (env, "hev/htproxy/TProxyService");
 	(*env)->RegisterNatives (env, klass, native_methods, N_ELEMENTS (native_methods));
 	(*env)->DeleteLocalRef (env, klass);
 
